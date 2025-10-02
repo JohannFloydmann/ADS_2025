0d9226237e4cb0a0e08607fa54e56c39053e6be3
JohannFd <samrikov.denis@gmail.com>
2025-10-02 22:03:27 +0500

    binary tree and lab4 task b

--- lectures/7_binary_tree.cpp
+++ lectures/7_binary_tree.cpp
@@ -1,53 +1,83 @@
 #include <iostream>
 using namespace std;
 
+
+
 class Node{
     private:
-        int data;
+        Node* parent;
         Node* left;
         Node* right;
-        Node* parent;
+        int data;
     public:
-        Node(int n){
-            data = n;
+        Node(int data){
+            parent = nullptr;
+            left = nullptr;
+            right = nullptr;
+            this->data = data;
         }
-
         int getData(){
             return data;
         }
 
-        Node* getRight(){
-            return right;
+        void setLeft(Node* newLeft){
+            left = newLeft;
+        }
+        void setRight(Node* newRight){
+            right = newRight;
+        }
+        void setParent(Node* newParent){
+            parent = newParent;
         }
 
         Node* getLeft(){
             return left;
         }
-
+        Node* getRight(){
+            return right;
+        }
         Node* getParent(){
             return parent;
         }
 
-        void setRight(Node* node){
-            right = node;
-        }
-
-        void setLeft(Node* node){
-            left = node;
-        }
-
-        void setParent(Node* node){
-            parent = node;
-        }
 };
 
-class BinaryTree{
+
+class BST{
     private:
         Node* root;
+
+        //helper:
+        void transplant(Node* u, Node* v){
+            Node* par = u->getParent(); 
+            if(par == NULL){
+                v = root;
+            } else if(u == par->getLeft()){
+                par->setLeft(v);
+            } else{
+                par->setRight(v);
+            }
+            if(v){
+                v->setParent(par);
+            }
+        }
+
+        Node* minimum(Node* pseudoroot){
+            Node* curr = pseudoroot;
+            Node* old_curr = NULL;
+            while(curr!=NULL){
+                old_curr = curr;
+                curr = curr->getLeft();
+            }
+
+            return old_curr;
+        }
     public:
-        BinaryTree(){
+
+        BST(){
             root = nullptr;
         }
+
         void insert(int newData){
             Node* newNode = new Node(newData);
 
@@ -55,7 +85,7 @@ class BinaryTree{
             Node* y = nullptr;
 
             while(x){
-                if(x->getData()<newData){
+                if(x->getData()>newData){
                     y = x;
                     x = x->getLeft();
                 }
@@ -82,7 +112,10 @@ class BinaryTree{
         void inOrderTreeWalk(Node* n){
             if(n){
                 inOrderTreeWalk(n->getLeft());
-                cout<<n->getData()<<" "<<n<<" ";
+                cout<<n->getData()<<" ";
+                if(n->getParent())
+                    cout<<n->getParent()->getData();
+                cout<<endl;
                 inOrderTreeWalk(n->getRight());
             }
         }
@@ -99,7 +132,7 @@ class BinaryTree{
             }
         }
 
-        ~BinaryTree(){
+        ~BST(){
             clear(root);
         }
 
@@ -123,11 +156,51 @@ class BinaryTree{
             }
             return NULL;
         }
-};
 
+        
+
+
+
+        void deleteNode(Node* z){
+            if(z->getLeft() == NULL){
+                this->transplant(z,z->getRight());
+            }
+            else if(z->getRight() == NULL){
+                this->transplant(z,z->getLeft());
+            }
+            else{
+                Node* y = minimum(z->getRight());
+                if(z->getRight()->getLeft() != NULL){
+                    y->setRight(z->getRight());
+                    z->getRight()->setParent(y);
+                    y->getRight()->setParent(z->getRight());
+                    this->transplant(y,y->getRight());
+                }
+                this->transplant(z,y);
+                y->setLeft(z->getLeft());
+
+            }
+            delete z;
+        }
+};
 
 int main(){
     
+    BST b1;
+
+    b1.insert(4);
+    b1.insert(3);
+    b1.insert(7);
+    b1.insert(2);
+    b1.insert(5);
+    b1.insert(8);
+
+    b1.print();
+
+    cout<<endl;
     
-    return 0;
+
+
+
+
 }
---
+++ practice/4lab/b.cpp
@@ -0,0 +1,272 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+private:
+    Node *parent;
+    Node *left;
+    Node *right;
+    int data;
+
+public:
+    Node(int data)
+    {
+        parent = nullptr;
+        left = nullptr;
+        right = nullptr;
+        this->data = data;
+    }
+    int getData()
+    {
+        return data;
+    }
+
+    void setLeft(Node *newLeft)
+    {
+        left = newLeft;
+    }
+    void setRight(Node *newRight)
+    {
+        right = newRight;
+    }
+    void setParent(Node *newParent)
+    {
+        parent = newParent;
+    }
+
+    Node *getLeft()
+    {
+        return left;
+    }
+    Node *getRight()
+    {
+        return right;
+    }
+    Node *getParent()
+    {
+        return parent;
+    }
+};
+
+class BST
+{
+private:
+    Node *root;
+
+    // helper:
+    void transplant(Node *u, Node *v)
+    {
+        Node *par = u->getParent();
+        if (par == NULL)
+        {
+            v = root;
+        }
+        else if (u == par->getLeft())
+        {
+            par->setLeft(v);
+        }
+        else
+        {
+            par->setRight(v);
+        }
+        if (v)
+        {
+            v->setParent(par);
+        }
+    }
+
+    Node *minimum(Node *pseudoroot)
+    {
+        Node *curr = pseudoroot;
+        Node *old_curr = NULL;
+        while (curr != NULL)
+        {
+            old_curr = curr;
+            curr = curr->getLeft();
+        }
+
+        return old_curr;
+    }
+
+public:
+    BST()
+    {
+        root = nullptr;
+    }
+
+    int count_children(Node *node)
+    {
+        if (!node)
+        {
+            return 0;
+        }
+
+        return 1 + count_children(node->getLeft()) + count_children(node->getRight());
+    }
+
+    void insert(int newData)
+    {
+        Node *newNode = new Node(newData);
+
+        Node *x = root;
+        Node *y = nullptr;
+
+        while (x)
+        {
+            if (x->getData() > newData)
+            {
+                y = x;
+                x = x->getLeft();
+            }
+            else
+            {
+                y = x;
+                x = x->getRight();
+            }
+        }
+
+        if (y == nullptr)
+        {
+            root = newNode;
+        }
+        else
+        {
+            if (newData < y->getData())
+            {
+                y->setLeft(newNode);
+            }
+            else
+            {
+                y->setRight(newNode);
+            }
+            newNode->setParent(y);
+        }
+    }
+
+    void inOrderTreeWalk(Node *n)
+    {
+        if (n)
+        {
+            inOrderTreeWalk(n->getLeft());
+            cout << n->getData() << " ";
+            if (n->getParent())
+                cout << n->getParent()->getData();
+            cout << endl;
+            inOrderTreeWalk(n->getRight());
+        }
+    }
+
+    void print()
+    {
+        inOrderTreeWalk(root);
+    }
+
+    void clear(Node *n)
+    {
+        if (n)
+        {
+            clear(n->getLeft());
+            clear(n->getRight());
+            delete n;
+        }
+    }
+
+    ~BST()
+    {
+        clear(root);
+    }
+
+    Node *search(int key)
+    {
+        return check(key, root);
+    }
+
+    Node *check(int key, Node *curr)
+    {
+        if (curr)
+        {
+            if (curr->getData() == key)
+            {
+                return curr;
+            }
+            if (curr->getData() < key)
+            {
+                return check(key, curr->getRight());
+                cout << "got here" << endl;
+            }
+            else
+            {
+                return check(key, curr->getLeft());
+            }
+        }
+        return NULL;
+    }
+
+    Node *searchByKey(int key)
+    {
+        Node *current = root;
+
+        while (current != nullptr && current->getData() != key)
+        {
+            if (key < current->getData())
+            {
+                current = current->getLeft();
+            }
+            else
+            {
+                current = current->getRight();
+            }
+        }
+
+        return current;
+    }
+
+    void deleteNode(Node *z)
+    {
+        if (z->getLeft() == NULL)
+        {
+            this->transplant(z, z->getRight());
+        }
+        else if (z->getRight() == NULL)
+        {
+            this->transplant(z, z->getLeft());
+        }
+        else
+        {
+            Node *y = minimum(z->getRight());
+            if (z->getRight()->getLeft() != NULL)
+            {
+                y->setRight(z->getRight());
+                z->getRight()->setParent(y);
+                y->getRight()->setParent(z->getRight());
+                this->transplant(y, y->getRight());
+            }
+            this->transplant(z, y);
+            y->setLeft(z->getLeft());
+        }
+        delete z;
+    }
+};
+
+int main()
+{
+
+    int n;
+    cin >> n;
+
+    BST b;
+
+    for (int i = 0; i < n; i++)
+    {
+        int temp;
+        cin >> temp;
+        b.insert(temp);
+    }
+
+    int find;
+    cin >> find;
+    cout << b.count_children(b.searchByKey(find));
+
+    cout << endl;
+}
